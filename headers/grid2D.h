2debff0261682d4ef13370675b694413659b02d2
José <ilbertofjunior@gmail.com>
2020-02-02 23:09:34 +0100

    Fixed some errors and must check if Eigen::all can be fixed by cloning the git repo

--- headers/grid2D.h
+++ headers/grid2D.h
@@ -4,21 +4,24 @@
 #include <Eigen/Dense>
 #include <vector>
 
+using namespace std;
 using namespace Eigen;
 
 namespace coarse_graining{
     class grid2D { 
     public:
-        double x, y, density, volFrac;
-        vector<int> neighbors;
-        static double areaTerm;        
+        double x, y, density, volFrac, areaTerm;
+        int nNeighbors;
+        vector<int> neighbors;        
         Vector2d momentum, velocity;
         Matrix2d kstress;
         bool momentum_check, velocity_check;
 
         grid2D();
         grid2D(double _x, double _y, double gridRes, vector<int> _neighbors);
-        ~grid2D();
+        virtual ~grid2D() {
+
+        }
 
         void density_field(const MatrixXd &data, double (*cg_function)(double, double, double, double));
         void momentum_field(const MatrixXd &data, double (*cg_function)(double, double, double, double));
--- src/grid2D.cpp
+++ src/grid2D.cpp
@@ -6,22 +6,20 @@ namespace coarse_graining{
     }
 
 
-    grid2D::grid2D(double _x, double _y, double gridRes, vector<int> _neighbors): x(_x), y(_y), areaTerm(gridRes*gridRes), momentum_check(false), velocity_check(false) {
-
-    }
-
-
-    virtual grid2D::~grid2D() {
-
+    grid2D::grid2D(double _x, double _y, double gridRes, vector<int> _neighbors): x(_x), y(_y), 
+    areaTerm(gridRes*gridRes), neighbors(_neighbors),  nNeighbors(_neighbors.size()), 
+    momentum_check(false), velocity_check(false) {
     }
 
 
     void grid2D::density_field(const MatrixXd &data,
             double (*cg_function)(double, double, double, double)) {
-        Matrix<T, neighbors.size(), Dynamic> relData = data(neighbors, all);
+        double phi;
+
+        Matrix<double, Dynamic, Dynamic> relData = data(neighbors, Eigen::all);
         density = 0;
         volFrac = 0;
-        for (int i = 0; i < neighbors.size(); i++){
+        for (int i = 0; i < nNeighbors; i++){
             phi = cg_function(x, y, relData(i, 1), relData(i, 2));
             volFrac += phi;
             density += relData(i, 0) * phi / areaTerm; //Mass of the particle should be in 1st column
@@ -32,13 +30,13 @@ namespace coarse_graining{
     void grid2D::momentum_field(const MatrixXd &data,
             double (*cg_function)(double, double, double, double)) {
 
-        Matrix<T, neighbors.size(), Dynamic> relData = data(neighbors, all);
-        double densityP;
+        Matrix<double, Dynamic, Dynamic> relData = data(neighbors, Eigen::all);
+        double densityP, phi;
         density = 0;
         volFrac = 0;
         momentum(0) = 0;
         momentum(1) = 0;
-        for (int i = 0; i < neighbors.size(); i++){
+        for (int i = 0; i < nNeighbors; i++){
             phi = cg_function(x, y, relData(i, 1), relData(i, 2));
             densityP = relData(i, 0) * phi / areaTerm;
             volFrac += phi;   
@@ -53,7 +51,7 @@ namespace coarse_graining{
     void grid2D::velocity_field(const MatrixXd &data, 
             double (*cg_function)(double, double, double, double)) {
 
-        if (!momentum_check) momentum_field(const MatrixXd &data, double (*cg_function)(double, double, double, double));
+        if (!momentum_check) momentum_field(data, *cg_function);
         velocity(0) = momentum(0) / density;
         velocity(1) = momentum(1) / density;
         velocity_check = true;
@@ -63,14 +61,14 @@ namespace coarse_graining{
     void grid2D::kstress_field(const MatrixXd &data, 
             double (*cg_function)(double, double, double, double)) {
 
-        if (!velocity_check) velocity_field(const MatrixXd &data, double (*cg_function)(double, double, double, double));
-        Matrix<T, neighbors.size(), Dynamic> relData = data(neighbors, all);
-        double densityP, vxx, vxy, vyy;
+        if (!velocity_check) velocity_field(data, *cg_function);
+        Matrix<double, Dynamic, Dynamic> relData = data(neighbors, Eigen::all);
+        double densityP, phi, vxx, vxy, vyy;
         kstress(0) = 0;
         kstress(1) = 0;
         kstress(2) = 0;
         kstress(3) = 0;
-        for (int i = 0; i < neighbors.size(); i++){
+        for (int i = 0; i < nNeighbors; i++){
             phi = cg_function(x, y, relData(i, 1), relData(i, 2));
             densityP = relData(i, 0) * phi / areaTerm;
             vxx = (relData(i, 3) - velocity(0)) * (relData(i, 3) - velocity(0));
