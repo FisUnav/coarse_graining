a508db239c69e63809c853316ed6904af6141d66
José <ilbertofjunior@gmail.com>
2020-02-03 08:07:06 +0100

    Now downloading Eigen directly from gitlab page and installing it from source code

--- requirements.sh
+++ requirements.sh
@@ -1,8 +1,8 @@
-wget https://gitlab.com/libeigen/eigen/-/archive/3.3.7/eigen-3.3.7.tar.bz2
-tar xvf eigen-3.3.7.tar.bz2
+git clone https://gitlab.com/libeigen/eigen
+mv eigen eigen_installation
 mkdir eigen
 cd eigen 
-cmake ../eigen-3.3.7
+cmake ../eigen_installation
 sudo make install
 cd ..
-sudo rm -R eigen eigen-3.3.7 eigen-3.3.7.tar.bz2
+sudo rm -R eigen eigen_installation
